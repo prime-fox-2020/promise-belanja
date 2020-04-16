b800d0412084877b925747513aa13e1c442b4c24
vinita-dewi <61119004+vinita-dewi@users.noreply.github.com>
2020-04-16 21:52:32 +0700

    solved belanja promise

--- beli.js
+++ beli.js
@@ -1,15 +1,29 @@
-function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
-  setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
-    }
-  }, obj.waktu);
-}
+// function beli(uang, obj, cb){
+//   console.log(`Saya pergi membeli ${obj.item}`)
+//   setTimeout(function(){
+//     let kembalian = uang - obj.harga
+//     if (kembalian > 0) {
+//       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
+//       cb(kembalian)
+//     }else{
+//       console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
+//       cb(uang)
+//     }
+//   }, obj.waktu);
+// }
 
+function beli (obj,uang){
+  return new Promise((res,rej) =>{
+    console.log(`Saya pergi membeli ${obj.item}`)
+    setTimeout(function(){
+      let kembalian = uang - obj.harga
+      if(kembalian > 0){
+        console.log(`Saya sudah membeli ${obj.item}. Uang kembaliannya adalah ${kembalian}`)
+        res(kembalian)
+      }else{
+        rej(uang)
+      }
+    })
+  },obj.waktu)
+}
 module.exports = beli;
