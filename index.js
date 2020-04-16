3b25bb22b03c899fa835fee052ed4e9d4b7d568d
fahrulnoer <fahrulnoer17@gmail.com>
2020-04-16 20:06:41 +0700

    change to promise

---
+++ index.js
@@ -0,0 +1,34 @@
+const beli = require('./beli')
+class Food {
+    constructor(item, harga, waktu) {
+        this.item = item
+        this.harga = harga
+        this.waktu = waktu
+    }
+}
+
+let bakwan = new Food('bakwan', 1000, 500)
+let tahu = new Food('tahu', 2000, 800)
+let martabak = new Food('martabak', 10000, 1000)
+let mie = new Food('mie', 7500, 600)
+let bakso = new Food('bakso', 8000, 400)
+
+beli(50000, bakwan)
+    .then((uang) => {
+        return beli(uang, tahu)
+    })
+    .then((uang) => {
+        return beli(uang, martabak)
+    })
+    .then((uang) => {
+        return beli(uang, mie)
+    })
+    .then((uang) => {
+        return beli(uang, bakso)
+    })
+    .then(()=>{
+        console.log(`Semua barang sudah dibeli`);
+    })
+    .catch((uang) => {
+        console.log(`uang kurang ${uang}`);
+    })
